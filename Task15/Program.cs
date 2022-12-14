39f222425844917ff3dff962a85537822156b97f
Kirich <kirya65@gmail.com>
2022-12-15 02:18:07 +0300

    Task 15 was accomlished

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число, обозначающий день недели чтобы увидеть является ли этот день выходным: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 5)
+    Console.Write("Да, это выходной день");
+else
+    while (n < 1 || n > 7) {
+        Console.Write("Число не обозначает день недели!\nВведите число, обозначающее день недели: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.Write("Нет, это будний день");
