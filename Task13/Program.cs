ad9e44688f8768a99151131630f4dd61b07e434e
Kirich <kirya65@gmail.com>
2022-12-15 02:09:18 +0300

    Task 13 was accomplished

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.Write("Введите число, чтобы увидеть его третью цифру: ");
+long digit = Convert.ToInt64(Console.ReadLine());
+string digitForRazryad = Convert.ToString(digit);
+long razryad = digitForRazryad.Length;
+long stepen = Convert.ToInt64(Math.Pow(10, razryad - 3));
+if (digit >= 100)
+    Console.WriteLine($"{(digit / stepen) % 10}");
+else
+    Console.WriteLine("У числа нет третьей цифры");
