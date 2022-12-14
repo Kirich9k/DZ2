d921e03643dea82984151f8ef1da92885a32e3b4
Kirich <kirya65@gmail.com>
2022-12-15 02:58:12 +0300

    Additional task was accomplished

--- AdditionalTask/Program.cs
+++ AdditionalTask/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int secondMax = 0;
+while (n != 0){
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max){
+        secondMax = max;
+        max = n;
+    }
+    else if (n > secondMax && n < max){
+        secondMax = n;
+    }
+}
+Console.WriteLine("End");
+Console.WriteLine();
+Console.WriteLine($"Second Max={secondMax}");
