c6a17dd0ac1a3e61a457947112d8939bff6f0d93
Kirich <kirya65@gmail.com>
2022-12-14 23:45:03 +0300

    Structure of DZ2 folder was created

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1 * n1 == n2 || n2 * n2 == n1)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
